e2f5daf304bb6a90ea6c98d324668daf7de9c8c8
Blake Pettersson <blake.pettersson@gmail.com>
2024-09-04 12:43:40 +0200

    feat: add pushsecret support for webhook
    This enables PushSecret functionality for webhooks

--- pkg/provider/webhook/webhook.go
+++ pkg/provider/webhook/webhook.go
@@ -15,10 +15,14 @@ limitations under the License.
 package webhook
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/external-secrets/external-secrets/pkg/constants"
+	"github.com/external-secrets/external-secrets/pkg/metrics"
+	"net/http"
 	"strconv"
 	"time"
 
@@ -114,9 +118,65 @@ func (w *WebHook) SecretExists(_ context.Context, _ esv1beta1.PushSecretRemoteRe
 	return false, errors.New(errNotImplemented)
 }
 
-// PushSecret not implement.
-func (w *WebHook) PushSecret(_ context.Context, _ *corev1.Secret, _ esv1beta1.PushSecretData) error {
-	return errors.New(errNotImplemented)
+func (w *WebHook) PushSecret(ctx context.Context, secret *corev1.Secret, data esv1beta1.PushSecretData) error {
+	if data.GetProperty() == "" && data.GetSecretKey() != "" {
+		return errors.New("requires property in RemoteRef to push secret value if secret key is defined")
+	}
+	provider, err := getProvider(w.store)
+	if err != nil {
+		return fmt.Errorf("failed to get store: %w", err)
+	}
+
+	method := provider.Method
+	if method == "" {
+		method = http.MethodPost
+	}
+
+	var value []byte
+	if data.GetSecretKey() == "" {
+		// Must convert secret values to string, otherwise data will be sent as base64
+		secretStringVal := make(map[string]string)
+		for k, v := range secret.Data {
+			secretStringVal[k] = string(v)
+		}
+		value, err = utils.JSONMarshal(secretStringVal)
+
+		if err != nil {
+			return fmt.Errorf("failed to serialize secret content as JSON: %w", err)
+		}
+	} else {
+		value = secret.Data[data.GetSecretKey()]
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, provider.URL, bytes.NewBuffer(value))
+	if err != nil {
+		return fmt.Errorf("failed to create http request: %w", err)
+	}
+
+	for k, v := range provider.Headers {
+		req.Header.Add(k, v)
+	}
+
+	resp, err := w.wh.HTTP.Do(req)
+	metrics.ObserveAPICall(constants.ProviderWebhook, constants.CallWebhookHTTPReq, err)
+	if err != nil {
+		return fmt.Errorf("failed to call endpoint: %w", err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode == 404 {
+		return esv1beta1.NoSecretError{}
+	}
+
+	if resp.StatusCode == http.StatusNotModified {
+		return esv1beta1.NotModifiedError{}
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("endpoint gave error %s", resp.Status)
+	}
+
+	return nil
 }
 
 // GetAllSecrets Empty .
